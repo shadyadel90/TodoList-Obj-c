3120190e20c871da0f0f6f31b0ded45225e84547
Shady Adel <shadyadel910@gmail.com>
2024-08-12 18:53:18 +0300

    archiving bug fixed

--- TodoListApp/TodoListApp/Todo.m
+++ TodoListApp/TodoListApp/Todo.m
@@ -9,4 +9,27 @@
 
 @implementation Todo
 
++ (BOOL)supportsSecureCoding {
+    return YES;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:self.name forKey:@"name"];
+    [coder encodeObject:self.descrption forKey:@"descrption"];
+    [coder encodeObject:self.priority forKey:@"priority"];
+    [coder encodeObject:self.status forKey:@"status"];
+    [coder encodeObject:self.date forKey:@"date"];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super init];
+    if (self) {
+        _name = [coder decodeObjectOfClass:[NSString class] forKey:@"name"];
+        _descrption = [coder decodeObjectOfClass:[NSString class] forKey:@"descrption"];
+        _priority = [coder decodeObjectOfClass:[NSString class] forKey:@"priority"];
+        _status = [coder decodeObjectOfClass:[NSString class] forKey:@"status"];
+        _date = [coder decodeObjectOfClass:[NSDate class] forKey:@"date"];
+    }
+    return self;
+}
 @end
